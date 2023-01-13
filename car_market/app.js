aeb847ccf963d761c6e74b65c217e725c4f07346
Michal Pluta <michalpl2003@gmail.com>
2023-01-13 00:04:00 +0000

    Implement GET request for accessing each page of cars

--- car_market/app.js
+++ car_market/app.js
@@ -16,6 +16,17 @@ app.get('/car/:id', function (req, resp) {
     // Return the first entry in the array in the unexpected case that two cars have the same id
     resp.send(filteredData[0]);
 });
+app.get('/cars', function (req, resp) {
+    const page_size = req.query.size    // How many items are displayed on each page
+    const page = req.query.page         // Which page of items should be displayed
+    
+    const offset = (page - 1) * page_size;  // Index of the first item to be displayed
+
+    // Slice the data so that it contains only the items on that specific page
+    let filteredData = car_market_data["cars"].slice(offset, offset + page_size)    
+    
+    resp.send(filteredData);
+});
 
 // Export app
 module.exports = app;
