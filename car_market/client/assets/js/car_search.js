07a4eabc180ec964f6a6d3b810a2d3e3375cfff2
Michal Pluta <michalpl2003@gmail.com>
2023-01-15 16:01:52 +0000

    Remove the need of storing HTML template as String

--- car_market/client/assets/js/car_search.js
+++ car_market/client/assets/js/car_search.js
@@ -6,34 +6,23 @@ async function listCars() {
     const data = JSON.parse(text)
 
     carListElt = document.getElementById('carList');
+
     var carCardTemplate = document.getElementById("carCardTemplate");
+    var templateContent = carCardTemplate.content
 
-    let carList = '';
     for (const carData of data) {
-        carList += `<div id="grid" class="col-12 col-lg-6 col-xxl-4">
-        <div id="card" class="card">
-            <img id="img" src="assets/images/loading.gif" alt="Image of a Toyota Yaris car" class="card-img-top card-car-img">
-            <div id="body" class="card-body">
-                <h5 id="title" class="card-title">${carData["make"] + " " + carData["model"]}</h5>
-                <p id="text" class="card-text">${"Color: " + carData["color"] + ", Price: £" + carData["price"]}</p>
-            </div>
-        </div>
-    </div>`
+        var copyHTML = document.importNode(templateContent, true);
 
-        // var clone = carCardTemplate.content;
-        // var card = clone.querySelector("div").querySelector("div")
-        // var body = card.getElementById("body")
-        // body.getElementById("title").innerHTML = carData["make"] + " " + carData["model"]
-        // body.getElementById("text").innerHTML = "Color: " + carData["color"] + ", Price: £" + parseString(carData["price"])
-
-        //  list += clone;
+        copyHTML.querySelector(".card-title").textContent = carData["make"] + " " + carData["model"];
+        copyHTML.querySelector(".card-text").textContent = "Color: " + carData["color"] + ", Price: £" + carData["price"];
+    
+        carListElt.appendChild(copyHTML);
     }
 
-     carListElt.innerHTML = carList;
-    // const listItems = document.querySelectorAll('.recipe_list_item');
-    // for (const listItem of listItems) {
-    //     listItem.addEventListener('click', (event) => loadRecipe(event.target.textContent));
-    // }
+    const listItems = carListElt.querySelectorAll('.card');
+    for (const listItem of listItems) {
+        listItem.addEventListener('click', (event) => console.log("click"));
+    }
 }
 
-document.addEventListener('DOMContentLoaded', listCars);
+ document.addEventListener('DOMContentLoaded', listCars);
