72924aa6bb18a96c1ef92194ce8d23a70fe10953
Michal Pluta <michalpl2003@gmail.com>
2023-01-22 19:28:33 +0100

    Make eslint run on app.js and car_search.js Fix all thrown eslint errors & warnings

--- honk_if_you_haggle/app.js
+++ honk_if_you_haggle/app.js
@@ -6,24 +6,24 @@ app.use(express.static(path.join(__dirname, 'client')));
 
 // Import data from honk_if_you_haggle database
 const fileNameForJSON = '.' + path.sep + 'honk_if_you_haggle_db.json';
-const car_market_data = require(fileNameForJSON);
+const carMarketData = require(fileNameForJSON);
 
 // Returns data about a specific car with a specific id
 app.get('/car/:id', function (req, resp) {
-    const req_id = req.params.id;   // ID of the car
-    
+    const reqID = req.params.id; // ID of the car
+
     // Filters data to only find the car with the given id
-    let filteredData = car_market_data["cars"].filter(function(car){
-        return car.id == req_id
+    const filteredData = carMarketData.cars.filter(function (car) {
+        return car.id === reqID;
     });
-    
+
     // Return the first entry in the array in the unexpected case that two cars have the same id
     resp.send(JSON.stringify(filteredData[0]));
 });
 
 // Returns data about all the cars
 app.get('/cars', function (req, resp) {
-    let filteredData = car_market_data["cars"]   
+    const filteredData = carMarketData.cars;
     resp.send(JSON.stringify(filteredData));
 });
 
